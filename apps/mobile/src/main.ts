ad07b3a561748ff6c2eb46ff29b0044d38c2dbfe
Danil Chigirev <danil.chigirev@gmail.com>
2026-01-28 00:08:41 +0700

    ionic app

--- apps/mobile/src/main.ts
+++ apps/mobile/src/main.ts
@@ -1,15 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { defineCustomElements } from '@ionic/pwa-elements/loader';
-
-import { appConfig } from './app/app.config';
+import { provideIonicAngular } from '@ionic/angular/standalone';
 import { App } from './app/app';
 
-defineCustomElements(window);
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
-
-// import { importProvidersFrom } from '@angular/core';
-// import { IonicModule } from '@ionic/angular';
-
-// bootstrapApplication(App, {
-//   providers: [importProvidersFrom(IonicModule.forRoot())],
-// }).catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [provideIonicAngular()],
+});
