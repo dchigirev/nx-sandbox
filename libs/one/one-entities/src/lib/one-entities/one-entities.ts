d6bb8ecf20d039be12c83d2bcdfba009f2f6b957
Danil Chigirev <danil.chigirev@gmail.com>
2026-01-31 15:31:04 +0700

    entities upd

--- libs/one/one-entities/src/lib/one-entities/one-entities.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-one-entities',
-  imports: [],
-  templateUrl: './one-entities.html',
-  styleUrl: './one-entities.css',
-})
-export class OneEntities {}
